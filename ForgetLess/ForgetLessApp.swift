da69216fc1c8d29b3c7b037d6404b5d36e58518f
Andrew Shapoval <andrew.shapoval@developex.net>
2025-02-02 14:29:04 +0200

    create project

---
+++ ForgetLess/ForgetLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgetLessApp.swift
+//  ForgetLess
+//
+//  Created by Andrew Shapoval on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ForgetLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForgetLessTests/ForgetLessTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForgetLessTests.swift
+//  ForgetLessTests
+//
+//  Created by Andrew Shapoval on 01/02/2025.
+//
+
+import Testing
+@testable import ForgetLess
+
+struct ForgetLessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
